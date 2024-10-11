dbbc18c19555e183bab95e5edf4da431b691cba6
grupobarao01 <af631577@gmail.com>
2024-10-11 11:34:33 -0300

    a

--- script.js
+++ script.js
@@ -94,11 +94,11 @@ document.getElementById('btn-resultados').addEventListener('click', () => {
                 </tr>
             </tbody>
         </table>
-        <p>Segunda (11): ${votos[11]} votos</p>
-        <p>Terca (22): ${votos[22]} votos</p>
-        <p>Quarta (33): ${votos[33]} votos</p>
-        <p>Quinta (44): ${votos[44]} votos</p>
-        <p>Sexta (55): ${votos[55]} votos</p>
+        <p>Ana(11): ${votos[11]} votos</p>
+        <p>Bruno (22): ${votos[22]} votos</p>
+        <p>Carla (33): ${votos[33]} votos</p>
+        <p>Daniel (44): ${votos[44]} votos</p>
+        <p>Elena (55): ${votos[55]} votos</p>
         <p>Votos em Branco: ${votos.branco}</p>
         <p>Votos Nulos: ${votos.nulo}</p>
     `;
